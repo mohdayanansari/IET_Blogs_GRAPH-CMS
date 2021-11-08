59db3b77d6f1f6c65b1f3d0a43101caeffc2a93e
Ayan Ansari <1100zaid@gmail.com>
2021-11-08 10:09:00 +0530

    chnages

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import { getCategories } from "../services";
-
+import logo from "../public/logo.png";
 
 const Header = () => {
   const [categories, setCategories] = useState([]);
@@ -13,12 +14,32 @@ const Header = () => {
   return (
     <div className="container px-10 mx-auto mb-8">
       <div className="flex items-center justify-between w-full py-8 border-b border-blue-400">
-        <div className="block ">
-          <Link href="/">
-            <span className="text-4xl font-bold text-white cursor-pointer">
-              IET Blogs
-            </span>
-          </Link>
+        <div className="flex flex-col gap-5">
+          <div className="flex flex-col items-center gap-4 md:flex-row">
+            <Link href="/" className="">
+              <Image
+                src={logo}
+                unoptimized
+                alt="logo"
+                height="70px"
+                width="70px"
+                className="cursor-pointer"
+              />
+            </Link>
+            <Link href="/">
+              <span className="flex flex-col items-center gap-1 text-2xl font-bold text-center text-transparent text-white cursor-pointer md:text-3xl md:text-left bg-clip-text bg-gradient-to-br from-red-400 to-yellow-600">
+                Institute of Engineering & Technology, Ayodhya <br />{" "}
+                <span className="text-xl font-medium text-white md:text-2xl">
+                  Blogs/Articles
+                </span>
+              </span>
+            </Link>
+          </div>
+          <h1 className="font-bold text-blue-500 transition duration-300 transform hover:underline hover:translate-x-3 hover:text-red-500">
+            <Link href="https://forms.gle/1M43Ff2zZ75g9cce6">
+              Apply for Blog/Article ✒️
+            </Link>
+          </h1>
         </div>
         <div>
           <div className="hidden md:contents ">
--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,19 @@ import { PostCard, PostWidget, Categories } from "../components";
 import { getPosts } from "../services";
 import { FeaturedPosts } from "../sections";
 
-
 export default function Home({ posts }) {
   return (
     <div className="container px-10 mx-auto mb-8">
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Institute of Enginerring & Technology, Ayodhya : Blogs/Articles  --Homepage" />
+        <meta name="author" content="Ayan Ansari" />
+        <meta name="author" content="Ayan" />
+        <meta property="og:site_name" content="Institute of Enginerring & Technology, Ayodhya : Blogs/Articles  --Homepage" />
+        <meta property="og:type" content="Website" />
+        <meta property="og:image" content="/metaimg.png" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <title>IET Blogs</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <FeaturedPosts />
